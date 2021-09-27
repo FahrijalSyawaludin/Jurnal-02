eca303b67686c4c8b612a60e4f2f4c8d328e73ec
Fahrijal Syawaludin <Fahrijals@telkomuniversity.ac.id>
2021-09-27 15:38:03 +0700

    Fahrijal.java

---
+++ Jurnal 01/Fahrijal.java
@@ -0,0 +1,9 @@
+public class Fahrijal {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Fahrijal!");
+		System.out.println("NIM: 6706213040");
+		System.out.println("Kelas: 45-03");
+    }
+
+}
